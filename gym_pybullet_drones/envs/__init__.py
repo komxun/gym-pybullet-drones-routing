d65edcabc31012322b5f3d772ac8742dd5f56d3a
komxun <komxun@gmail.com>
2024-06-25 12:05:44 +0100

    updated __init__.py in envs package

--- gym_pybullet_drones/envs/__init__.py
+++ gym_pybullet_drones/envs/__init__.py
@@ -2,3 +2,6 @@ from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
 from gym_pybullet_drones.envs.DynAviary import DynAviary
 from gym_pybullet_drones.envs.VelocityAviary import VelocityAviary
 from gym_pybullet_drones.envs.VisionAviary import VisionAviary
+
+# New
+from gym_pybullet_drones.envs.RoutingAviary import RoutingAviary
