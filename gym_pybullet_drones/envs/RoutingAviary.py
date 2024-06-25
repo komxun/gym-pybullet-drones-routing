112a5f63035683282cd739b45a5efe867f9b1ec2
komxun <komxun@gmail.com>
2024-06-25 12:02:41 +0100

    added RoutingAviary.py

---
+++ gym_pybullet_drones/envs/RoutingAviary.py
@@ -0,0 +1,254 @@
+import numpy as np
+from gym import spaces
+import pybullet as p
+from gym_pybullet_drones.envs.BaseAviary import DroneModel, Physics, BaseAviary
+from gym_pybullet_drones.envs.CtrlAviary import CtrlAviary
+
+class RoutingAviary(CtrlAviary):
+    """Single-drone environment class for routing application"""
+    
+    OBSTACLE_IDS = []
+    
+    def __init__(self, 
+                 drone_model: DroneModel=DroneModel.CF2X,
+                 num_drones: int=1,
+                 neighbourhood_radius: float=np.inf,
+                 initial_xyzs=None,
+                 initial_rpys=None,
+                 physics: Physics=Physics.PYB,
+                 freq: int=240,
+                 aggregate_phy_steps: int=1,
+                 gui=False,
+                 record=False,
+                 obstacles=False,
+                 user_debug_gui=True
+                 ):
+        """Initialization of an aviary environment for routing applications.
+        
+        Parameters
+        ----------
+        drone_model : DroneModel, optional
+            The desired drone type (detailed in an .urdf file in folder `assets`).
+        num_drones : int, optional
+            The desired number of drones in the aviary.
+        neighbourhood_radius : float, optional
+            Radius used to compute the drones' adjacency matrix, in meters.
+        initial_xyzs: ndarray | None, optional
+            (NUM_DRONES, 3)-shaped array containing the initial XYZ position of the drones.
+        initial_rpys: ndarray | None, optional
+            (NUM_DRONES, 3)-shaped array containing the initial orientations of the drones (in radians).
+        physics : Physics, optional
+            The desired implementation of PyBullet physics/custom dynamics.
+        freq : int, optional
+            The frequency (Hz) at which the physics engine steps.
+        aggregate_phy_steps : int, optional
+            The number of physics steps within one call to `BaseAviary.step()`.
+        gui : bool, optional
+            Whether to use PyBullet's GUI.
+        record : bool, optional
+            Whether to save a video of the simulation in folder `files/videos/`.
+        obstacles : bool, optional
+            Whether to add obstacles to the simulation.
+        user_debug_gui : bool, optional
+            Whether to draw the drones' axes and the GUI RPMs sliders.
+        
+        """
+        super().__init__(drone_model=drone_model,
+                         num_drones=num_drones,
+                         neighbourhood_radius=neighbourhood_radius,
+                         initial_xyzs=initial_xyzs,
+                         initial_rpys=initial_rpys,
+                         physics=physics,
+                         freq=freq,
+                         aggregate_phy_steps=aggregate_phy_steps,
+                         gui=gui,
+                         record=record,
+                         obstacles=obstacles,
+                         user_debug_gui=user_debug_gui
+                         )
+        #### Set a limit on the maximum target speed ###############
+        speedLimitingFactor = 0.5   # 0.03
+        self.SPEED_LIMIT = speedLimitingFactor * self.MAX_SPEED_KMH * (1000/3600)
+        
+        # # New ---------Get obstacle information
+        self._getObstaclesData()
+    
+    ################################################################################
+    # Adding more function to step()
+    def step(self, action):
+        """Advances the environment by one simulation step.
+
+        Parameters
+        ----------
+        action : ndarray | dict[..]
+            The input action for one or more drones, translated into RPMs by
+            the specific implementation of `_preprocessAction()` in each subclass.
+
+        Returns
+        -------
+        ndarray | dict[..]
+            The step's observation, check the specific implementation of `_computeObs()`
+            in each subclass for its format.
+        float | dict[..]
+            The step's reward value(s), check the specific implementation of `_computeReward()`
+            in each subclass for its format.
+        bool | dict[..]
+            Whether the current epoisode is over, check the specific implementation of `_computeDone()`
+            in each subclass for its format.
+        dict[..]
+            Additional information as a dictionary, check the specific implementation of `_computeInfo()`
+            in each subclass for its format.
+
+        """
+        #### Save PNG video frames if RECORD=True and GUI=False ####
+        if self.RECORD and not self.GUI and self.step_counter%self.CAPTURE_FREQ == 0:
+            [w, h, rgb, dep, seg] = p.getCameraImage(width=self.VID_WIDTH,
+                                                     height=self.VID_HEIGHT,
+                                                     shadow=1,
+                                                     viewMatrix=self.CAM_VIEW,
+                                                     projectionMatrix=self.CAM_PRO,
+                                                     renderer=p.ER_TINY_RENDERER,
+                                                     flags=p.ER_SEGMENTATION_MASK_OBJECT_AND_LINKINDEX,
+                                                     physicsClientId=self.CLIENT
+                                                     )
+            (Image.fromarray(np.reshape(rgb, (h, w, 4)), 'RGBA')).save(self.IMG_PATH+"frame_"+str(self.FRAME_NUM)+".png")
+            #### Save the depth or segmentation view instead #######
+            # dep = ((dep-np.min(dep)) * 255 / (np.max(dep)-np.min(dep))).astype('uint8')
+            # (Image.fromarray(np.reshape(dep, (h, w)))).save(self.IMG_PATH+"frame_"+str(self.FRAME_NUM)+".png")
+            # seg = ((seg-np.min(seg)) * 255 / (np.max(seg)-np.min(seg))).astype('uint8')
+            # (Image.fromarray(np.reshape(seg, (h, w)))).save(self.IMG_PATH+"frame_"+str(self.FRAME_NUM)+".png")
+            self.FRAME_NUM += 1
+        #### Read the GUI's input parameters #######################
+        if self.GUI and self.USER_DEBUG:
+            current_input_switch = p.readUserDebugParameter(self.INPUT_SWITCH, physicsClientId=self.CLIENT)
+            if current_input_switch > self.last_input_switch:
+                self.last_input_switch = current_input_switch
+                self.USE_GUI_RPM = True if self.USE_GUI_RPM == False else False
+        if self.USE_GUI_RPM:
+            for i in range(4):
+                self.gui_input[i] = p.readUserDebugParameter(int(self.SLIDERS[i]), physicsClientId=self.CLIENT)
+            clipped_action = np.tile(self.gui_input, (self.NUM_DRONES, 1))
+            if self.step_counter%(self.SIM_FREQ/2) == 0:
+                self.GUI_INPUT_TEXT = [p.addUserDebugText("Using GUI RPM",
+                                                          textPosition=[0, 0, 0],
+                                                          textColorRGB=[1, 0, 0],
+                                                          lifeTime=1,
+                                                          textSize=2,
+                                                          parentObjectUniqueId=self.DRONE_IDS[i],
+                                                          parentLinkIndex=-1,
+                                                          replaceItemUniqueId=int(self.GUI_INPUT_TEXT[i]),
+                                                          physicsClientId=self.CLIENT
+                                                          ) for i in range(self.NUM_DRONES)]
+        #### Save, preprocess, and clip the action to the max. RPM #
+        else:
+            self._saveLastAction(action)
+            clipped_action = np.reshape(self._preprocessAction(action), (self.NUM_DRONES, 4))
+        #### Repeat for as many as the aggregate physics steps #####
+        for _ in range(self.AGGR_PHY_STEPS):
+            #### Update and store the drones kinematic info for certain
+            #### Between aggregate steps for certain types of update ###
+            if self.AGGR_PHY_STEPS > 1 and self.PHYSICS in [Physics.DYN, Physics.PYB_GND, Physics.PYB_DRAG, Physics.PYB_DW, Physics.PYB_GND_DRAG_DW]:
+                self._updateAndStoreKinematicInformation()
+            #### Step the simulation using the desired physics update ##
+            for i in range (self.NUM_DRONES):
+                if self.PHYSICS == Physics.PYB:
+                    self._physics(clipped_action[i, :], i)
+                elif self.PHYSICS == Physics.DYN:
+                    self._dynamics(clipped_action[i, :], i)
+                elif self.PHYSICS == Physics.PYB_GND:
+                    self._physics(clipped_action[i, :], i)
+                    self._groundEffect(clipped_action[i, :], i)
+                elif self.PHYSICS == Physics.PYB_DRAG:
+                    self._physics(clipped_action[i, :], i)
+                    self._drag(self.last_clipped_action[i, :], i)
+                elif self.PHYSICS == Physics.PYB_DW:
+                    self._physics(clipped_action[i, :], i)
+                    self._downwash(i)
+                elif self.PHYSICS == Physics.PYB_GND_DRAG_DW:
+                    self._physics(clipped_action[i, :], i)
+                    self._groundEffect(clipped_action[i, :], i)
+                    self._drag(self.last_clipped_action[i, :], i)
+                    self._downwash(i)
+            #### PyBullet computes the new state, unless Physics.DYN ###
+            if self.PHYSICS != Physics.DYN:
+                p.stepSimulation(physicsClientId=self.CLIENT)
+            #### Save the last applied action (e.g. to compute drag) ###
+            self.last_clipped_action = clipped_action
+        #### Update and store the drones kinematic information #####
+        self._updateAndStoreKinematicInformation()
+        #### Prepare the return values #############################
+        obs = self._computeObs()
+        reward = self._computeReward()
+        done = self._computeDone()
+        info = self._computeInfo()
+        
+        # NEW ***  Process Obstacles' data ************************
+        # if self.OBSTACLES:
+        #     self._getObstaclesData()
+        #### Advance the step counter ##############################
+        self.step_counter = self.step_counter + (1 * self.AGGR_PHY_STEPS)
+        
+        return obs, reward, done, info
+
+    ################################################################################
+        
+    def _addObstacles(self):
+        """Add obstacles to the environment.
+
+        These obstacles are loaded from standard URDF files included in Bullet.
+
+        """
+        RoutingAviary.OBSTACLE_IDS.append(
+                           p.loadURDF("samurai.urdf",
+                           physicsClientId=self.CLIENT
+                           ))
+
+        RoutingAviary.OBSTACLE_IDS.append(
+            p.loadURDF("cube_no_rotation.urdf",
+                   [-.5, -2.5, .5],
+                   p.getQuaternionFromEuler([0, 0, 0]),
+                   physicsClientId=self.CLIENT
+                   ))
+        RoutingAviary.OBSTACLE_IDS.append(
+            p.loadURDF("sphere2.urdf",
+                   [0, 2, .5],
+                   p.getQuaternionFromEuler([0,0,0]),
+                   useFixedBase = True, 
+                   physicsClientId=self.CLIENT
+                   ))
+        RoutingAviary.OBSTACLE_IDS.append(
+            p.loadURDF("sphere2.urdf", 
+                   [0.8, 5, 0+1], 
+                   p.getQuaternionFromEuler([0,0,0]), 
+                   useFixedBase = True, 
+                   physicsClientId=self.CLIENT))
+        RoutingAviary.OBSTACLE_IDS.append(
+            p.loadURDF("sphere2.urdf", 
+                   [0.5, 3, 0.5], 
+                   p.getQuaternionFromEuler([0,0,0]), 
+                   useFixedBase = True, 
+                   physicsClientId=self.CLIENT))
+        RoutingAviary.OBSTACLE_IDS.append(
+            p.loadURDF("sphere2.urdf", 
+                   [0.8, 7, 0], 
+                   p.getQuaternionFromEuler([0,0,0]), 
+                   useFixedBase = True, 
+                   physicsClientId=self.CLIENT))
+        # self._getObstaclesData()
+ 
+
+        
+    def _getObstaclesData(self):
+        idsList = RoutingAviary.OBSTACLE_IDS
+        obs_pos = []
+        obs_size = []
+        for j in range(len(idsList)):
+            pos, orn = p.getBasePositionAndOrientation(idsList[j])
+
+            obs_pos.append(pos)
+            vsd = p.getVisualShapeData(idsList[j])
+            obs_size.append(vsd[0][3])
+        
+        self.obs_size = np.array(obs_size)  # matrix of size Nx3
+        self.obs_pos = np.array(obs_pos)    # matrix of size Nx3
+        
