5b2baece48c6fdfb1009626893b6c25ae09d01f5
komsun@lap4072 <komxun@gmail.com>
2026-01-15 11:40:11 +0000

    added HB drone model option

--- gym_pybullet_drones/utils/enums.py
+++ gym_pybullet_drones/utils/enums.py
@@ -5,6 +5,7 @@ class DroneModel(Enum):
 
     CF2X = "cf2x"   # Bitcraze Craziflie 2.0 in the X configuration
     CF2P = "cf2p"   # Bitcraze Craziflie 2.0 in the + configuration
+    HB = "hb"       # Generic quadrotor (with AscTec Hummingbird inertial properties)
     RACE = "racer"  # Racer drone in the X configuration
 
 
@@ -39,6 +40,7 @@ class ActionType(Enum):
     VEL = "vel"                 # Velocity input (using PID control)
     ONE_D_RPM = "one_d_rpm"     # 1D (identical input to all motors) with RPMs
     ONE_D_PID = "one_d_pid"     # 1D (identical input to all motors) with PID control
+    TUN = "tun" 
     # New Action
     AUTOROUTING = "autorouting"  # Route selection with speed adjustment
     
